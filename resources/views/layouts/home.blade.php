d62681ebaade8ce3ed6a35a4bd58f6180c4eb133
Matthew Glinski <matthewglinski@gmail.com>
2015-04-23 15:38:04 -0400

    Moar template updates

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -2,44 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>HERO Timers</title>
+    <title>Brave Collective Forums Auth</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="<?=URL::asset('css/bootstrap.css')?>">
-	<link rel="stylesheet" href="<?=URL::asset('css/sticky.css')?>">
-	<link rel="stylesheet" href="<?=URL::asset('css/util.css')?>">
-	<link rel="stylesheet" href="<?=URL::asset('css/select2.css')?>">
-	<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/smoothness/jquery-ui.min.css">
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/alertify.js/0.3.10/alertify.core.css">
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/alertify.js/0.3.10/alertify.default.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-	<script src="<?=URL::asset('js/bootstrap.min.js')?>"></script>
-	<script src="<?=URL::asset('js/jquery.moment.js');?>"></script>
-	<script src="<?=URL::asset('js/livestamp.js');?>"></script>
-	<script src="<?=URL::asset('js/select2.min.js')?>"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/alertify.js/0.3.10/alertify.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
-    <style>
+    <style type="text/css">
         .jumbotron {
 	        margin-top: 30px;
         }
-
-        .ui-timepicker-div .ui-widget-header { margin-bottom: 8px; }
-        .ui-timepicker-div dl { text-align: left; }
-        .ui-timepicker-div dl dt { float: left; clear:left; padding: 0 0 0 5px; }
-        .ui-timepicker-div dl dd { margin: 0 10px 10px 40%; }
-        .ui-timepicker-div td { font-size: 90%; }
-        .ui-tpicker-grid-label { background: none; border: none; margin: 0; padding: 0; }
-
-        .ui-timepicker-rtl{ direction: rtl; }
-        .ui-timepicker-rtl dl { text-align: right; padding: 0 5px 0 0; }
-        .ui-timepicker-rtl dl dt{ float: right; clear: right; }
-        .ui-timepicker-rtl dl dd { margin: 0 40% 10px 10px; }
     </style>
 </head>
 <body>
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -2,11 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Login - HERO Timers</title>
+	<title>Login - Brave Collective Forums Auth</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<!-- Latest compiled and minified CSS -->
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
@@ -14,39 +13,39 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
-	<style>
-		.jumbotron {
-			margin-top: 30px;
-		}
-	</style>
+    <style type="text/css">
+        .jumbotron {
+            margin-top: 30px;
+        }
+    </style>
 </head>
 <body>
 
-<!-- Wrap all page content here -->
-<div id="wrap">
-	<?php
-	/*
-	 * Load the page content here
-	 */
-	echo $navigation;
-	?>
+    <!-- Wrap all page content here -->
+    <div id="wrap">
+        <?php
+        /*
+         * Load the page content here
+         */
+        echo $navigation;
+        ?>
 
-	<div class="container">
-		<?php
-		/*
-		 * Load the page content here
-		 */
-		echo $page_content;
-		?>
-	</div> <!-- /container -->
-</div>
+        <div class="container">
+            <?php
+            /*
+             * Load the page content here
+             */
+            echo $page_content;
+            ?>
+        </div> <!-- /container -->
+    </div>
 
-<?php
-/*
- * Load the page content here
- */
-echo $footer;
-?>
+    <?php
+    /*
+     * Load the page content here
+     */
+    echo $footer;
+    ?>
 
 </body>
 </html>
